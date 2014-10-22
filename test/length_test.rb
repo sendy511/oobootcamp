9b77cf5fc1272abb3b8f4f8454952aeeec9d717a
Jason-Home <me@yansen.com>
2014-10-22 23:50:47 +0800

    support length equal

--- test/length_test.rb
+++ test/length_test.rb
@@ -5,5 +5,14 @@ class LengthTest < Test::Unit::TestCase
 	def test_empty
 	end
 
-	def test
+	def test_length_exist
+		length = Length.new
+		assert_not_nil length
+	end
+
+	def test_lenght_can_equal
+		length1 = Length.new(1)
+		length2 = Length.new(1)
+		assert_equal(length1, length2)
+	end
 end
