a2b779ea72bbec6a9d780f7449fd5992015284d4
Yansen <Yansen@test.com>
2014-10-28 22:23:12 +0800

    rename

--- parkinglot/parking_helper.rb
+++ parkinglot/parking_helper.rb
@@ -1,13 +1,13 @@
 class ParkingHelper
 	attr_accessor :parked_cars_with_parkinglot
-	attr_accessor :parkinglot_list
+	attr_accessor :managed_parkinglots
 
 	def initialize(managed_parkinglots = [])
-		@parkinglot_list = []
+		@managed_parkinglots = []
 		if managed_parkinglots.length == 0 then
-			@parkinglot_list.push Parkinglot.new
+			@managed_parkinglots.push Parkinglot.new
 		else
-			@parkinglot_list = managed_parkinglots
+			@managed_parkinglots = managed_parkinglots
 		end
 
 		@parked_cars_with_parkinglot = {}
@@ -15,10 +15,10 @@ class ParkingHelper
 
 	def park(car)
 		@parked_cars_with_parkinglot[car.id] = 1;
-		return @parkinglot_list[0].park(car)
+		return @managed_parkinglots[0].park(car)
 	end
 
 	def pick(car_id)
-		return @parkinglot_list[0].pick(car_id)
+		return @managed_parkinglots[0].pick(car_id)
 	end
 end
