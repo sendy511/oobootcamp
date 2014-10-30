cef612ab8a56bf4e9ae0a0ce3de54372311ccc11
Yansen <Yansen@test.com>
2014-10-30 18:05:49 +0800

    reorder test cases

--- parkinglot/spec/parkinglot_spec.rb
+++ parkinglot/spec/parkinglot_spec.rb
@@ -7,10 +7,24 @@ describe "parkinglot" do
 		expect(parkinglot.park(car1)).to eq(true)
 	end
 
+	it "should be able to pick parked car" do
+		parkinglot = Parkinglot.new
+		car1 = Car.new("GA-888888")
+		parkinglot.park(car1)
+		expect(parkinglot.pick(car1.id)).to eq(car1)
+	end
+
 	it "should not be able to park car when full" do
 		parkinglot = Parkinglot.new(0)
 		car1 = Car.new
 		expect(parkinglot.park(car1)).to eq(false)
+	end
+
+	it "should not be able to pick an unpacked car" do
+		parkinglot = Parkinglot.new
+		car1 = Car.new("GA-888888")
+		parkinglot.park(car1)
+		expect(parkinglot.pick("GA-000000")).to eq(false)
 	end
 
 	it "should not be able to pick car again after picked" do
@@ -32,21 +46,6 @@ describe "parkinglot" do
 		expect(parkinglot.park(car2)).to eq(true)
 
 	end
-
-	it "should be able to pick parked car" do
-		parkinglot = Parkinglot.new
-		car1 = Car.new("GA-888888")
-		parkinglot.park(car1)
-		expect(parkinglot.pick(car1.id)).to eq(car1)
-	end
-
-	it "should not be able to pick an unpacked car" do
-		parkinglot = Parkinglot.new
-		car1 = Car.new("GA-888888")
-		parkinglot.park(car1)
-		expect(parkinglot.pick("GA-000000")).to eq(false)
-	end
-
 end
 
 describe "parking_helper" do
