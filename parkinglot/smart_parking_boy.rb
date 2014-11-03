385d8090f303185aa84069532fe954983f06a585
Yansen <Yansen@test.com>
2014-11-03 23:14:04 +0800

    can choose biggest parking lot

--- parkinglot/smart_parking_boy.rb
+++ parkinglot/smart_parking_boy.rb
@@ -1,4 +1,33 @@
-class SmartParkingBoy < ParkingHelper
+require 'ruby-debug'
 
+class SmartParkingBoy
 
+	attr_accessor :managed_parkinglots
+
+	def initialize(managed_parkinglots = [])
+		@managed_parkinglots = []
+		if managed_parkinglots.length == 0 then
+			@managed_parkinglots.push Parkinglot.new
+		else
+			@managed_parkinglots = managed_parkinglots
+		end
+	end
+
+	def park(car)
+		biggestParkinglot = @managed_parkinglots.max_by do |element|
+			element.capacity
+		end
+		return biggestParkinglot.park(car)
+	end
+
+	def pick(car_id)
+		@managed_parkinglots.each do |parkinglot|
+			pick_car_result = parkinglot.pick(car_id)
+			if pick_car_result then
+				return pick_car_result
+			end
+		end
+
+		return false
+	end
 end
--- parkinglot/spec/smart_parking_boy_spec.rb
+++ parkinglot/spec/smart_parking_boy_spec.rb
@@ -1,6 +1,78 @@
 require_relative '../rspec_helper'
 
 describe "Smart Parking Boy" do
+	it "should be able park car" do
+		parking_service = SmartParkingBoy.new
+		car = Car.new
+		expect(parking_service.park(car)).to eq(true)
+	end
+
+	it "should be able to pick parked car" do
+		parking_service = SmartParkingBoy.new
+		car = Car.new
+		parking_service.park(car)
+		expect(parking_service.pick(car.id)).to eq(car)
+	end
+
+	it "should not be able to park when all parking lots are full" do
+		parkinglot1 = Parkinglot.new(0)
+		parkinglot2 = Parkinglot.new(0)
+		parking_service = SmartParkingBoy.new([parkinglot1, parkinglot2])
+
+		car = Car.new
+
+		expect(parking_service.park(car)).to eq(false)
+	end
+
+	it "should be able to pick a car which is parked by car owner" do
+		parkinglot = Parkinglot.new
+		car = Car.new
+		parkinghelper = SmartParkingBoy.new([parkinglot])
+
+		parkinglot.park(car)
+
+		expect(parkinghelper.pick(car.id)).to eq(car)
+	end
+
+	it "should not be able to pick a car which is never parked" do
+		parkinghelper = SmartParkingBoy.new
+		expect(parkinghelper.pick("GA000000")).to eq(false)
+	end
+
+	it "should not be able to pick a car again after picked" do
+		parkinghelper = SmartParkingBoy.new
+		car = Car.new
+		parkinghelper.park(car)
+
+		parkinghelper.pick(car.id)
+
+		expect(parkinghelper.pick(car.id)).to eq(false)
+	end
+
+	# it "should still be able to park when only one is full but it manages two parking lots" do
+	# 	parkinglot1 = Parkinglot.new(1)
+	# 	parkinglot2 = Parkinglot.new(1)
+	# 	parkinghelper = SmartParkingBoy.new([parkinglot1, parkinglot2])
+	# 	car1 = Car.new
+	# 	parkinghelper.park(car1)
+
+	# 	car2 = Car.new
+	# 	expect(parkinghelper.park(car2)).to eq(true)
+	# end
+
+	# it "should be able to pick correct cars when there are multiple parkinglots" do
+	# 	parkinglot1 = Parkinglot.new(1)
+	# 	parkinglot2 = Parkinglot.new(1)
+	# 	parkinghelper = SmartParkingBoy.new([parkinglot1, parkinglot2])
+	# 	car1 = Car.new
+	# 	parkinghelper.park(car1)
+	# 	car2 = Car.new
+	# 	parkinghelper.park(car2)
+
+	# 	expect(parkinghelper.pick(car1.id)).to eq(car1)
+	# 	expect(parkinghelper.pick(car2.id)).to eq(car2)
+	# end
+
 	describe "When there is only one parking lot" do
 		it "should park to this parking lot" do
 			parkinglot = Parkinglot.new
@@ -8,6 +80,17 @@ describe "Smart Parking Boy" do
 			car = Car.new
 			smartParkingBoy.park(car)
 			expect(parkinglot.pick(car.id)).to eq(car)
+		end
+	end
+
+	describe "When there are two empty parking lots" do
+		it "should park to the bigger parking lots" do
+			parkinglot = Parkinglot.new(1)
+			biggerParkinglot = Parkinglot.new(2)
+			car = Car.new
+			smartParkingBoy = SmartParkingBoy.new([parkinglot, biggerParkinglot])
+			smartParkingBoy.park(car)
+			expect(biggerParkinglot.pick(car.id)).to eq(car)
 		end
 	end
 end
