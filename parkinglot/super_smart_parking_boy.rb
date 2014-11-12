87ef00c6c11f98c06d8b7eec3b084c47fe7da906
Yansen <Yansen@test.com>
2014-11-12 22:50:35 +0800

    refactor super smart parking boy

--- parkinglot/super_smart_parking_boy.rb
+++ parkinglot/super_smart_parking_boy.rb
@@ -1,12 +1,11 @@
 require 'ruby-debug'
 
 class SuperSmartParkingBoy < ParkingBoy
-	def park(car)
-		biggestParkinglot = (@managed_parkinglots.sort_by {
-			|parkinglot| -(parkinglot.left_spaces)
-		}).max_by do |element|
-			element.spare_rate
-		end
-		return biggestParkinglot.park(car)
+	def retrieve_satified_parkinglot
+		(@managed_parkinglots.sort_by {
+				|parkinglot| -(parkinglot.left_spaces)
+			}).max_by do |element|
+				element.spare_rate
+			end
 	end
 end
