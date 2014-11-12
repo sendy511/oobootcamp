43b258f986fca56f9d5bba504c0cb5859eccf8d2
Yansen <Yansen@test.com>
2014-11-12 22:26:09 +0800

    refactor the SuperSmartBoy to inherate from ParkingHelper

--- parkinglot/super_smart_boy.rb
+++ parkinglot/super_smart_boy.rb
@@ -1,18 +1,6 @@
 require 'ruby-debug'
 
-class SuperSmartBoy
-
-	attr_accessor :managed_parkinglots
-
-	def initialize(managed_parkinglots = [])
-		@managed_parkinglots = []
-		if managed_parkinglots.length == 0 then
-			@managed_parkinglots.push Parkinglot.new
-		else
-			@managed_parkinglots = managed_parkinglots
-		end
-	end
-
+class SuperSmartBoy < ParkingHelper
 	def park(car)
 		biggestParkinglot = (@managed_parkinglots.sort_by {
 			|parkinglot| -(parkinglot.left_spaces)
@@ -20,16 +8,5 @@ class SuperSmartBoy
 			element.spare_rate
 		end
 		return biggestParkinglot.park(car)
-	end
-
-	def pick(car_id)
-		@managed_parkinglots.each do |parkinglot|
-			pick_car_result = parkinglot.pick(car_id)
-			if pick_car_result then
-				return pick_car_result
-			end
-		end
-
-		return false
 	end
 end
