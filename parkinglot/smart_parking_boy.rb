562902174dea7cf474b7cc650b3efe59eb850ddb
Yansen <Yansen@test.com>
2014-11-12 22:24:38 +0800

    refactor smart parking boy as inherence to parking_helper

--- parkinglot/smart_parking_boy.rb
+++ parkinglot/smart_parking_boy.rb
@@ -1,33 +1,10 @@
 require 'ruby-debug'
 
-class SmartParkingBoy
-
-	attr_accessor :managed_parkinglots
-
-	def initialize(managed_parkinglots = [])
-		@managed_parkinglots = []
-		if managed_parkinglots.length == 0 then
-			@managed_parkinglots.push Parkinglot.new
-		else
-			@managed_parkinglots = managed_parkinglots
-		end
-	end
-
+class SmartParkingBoy < ParkingHelper
 	def park(car)
 		biggestParkinglot = @managed_parkinglots.max_by do |element|
 			element.left_spaces
 		end
 		return biggestParkinglot.park(car)
-	end
-
-	def pick(car_id)
-		@managed_parkinglots.each do |parkinglot|
-			pick_car_result = parkinglot.pick(car_id)
-			if pick_car_result then
-				return pick_car_result
-			end
-		end
-
-		return false
 	end
 end
