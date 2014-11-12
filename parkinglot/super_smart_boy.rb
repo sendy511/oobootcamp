1cb9635b6099f97ac643e0cb780c4411ff2bb4ca
Yansen <Yansen@test.com>
2014-11-12 12:25:35 +0800

    re-assign lines

--- parkinglot/super_smart_boy.rb
+++ parkinglot/super_smart_boy.rb
@@ -14,7 +14,9 @@ class SuperSmartBoy
 	end
 
 	def park(car)
-		biggestParkinglot = (@managed_parkinglots.sort_by {|parkinglot| -(parkinglot.left_spaces)}).max_by do |element|
+		biggestParkinglot = (@managed_parkinglots.sort_by {
+			|parkinglot| -(parkinglot.left_spaces)
+		}).max_by do |element|
 			element.spare_rate
 		end
 		return biggestParkinglot.park(car)
