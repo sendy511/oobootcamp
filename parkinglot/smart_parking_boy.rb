5863c7c34caceb133ff2ac445d0ba8cec01242e8
Yansen <Yansen@test.com>
2014-11-12 22:49:21 +0800

    refactor smart parking boy

--- parkinglot/smart_parking_boy.rb
+++ parkinglot/smart_parking_boy.rb
@@ -1,10 +1,9 @@
 require 'ruby-debug'
 
 class SmartParkingBoy < ParkingBoy
-	def park(car)
-		biggestParkinglot = @managed_parkinglots.max_by do |element|
+	def retrieve_satified_parkinglot
+		@managed_parkinglots.max_by do |element|
 			element.left_spaces
 		end
-		return biggestParkinglot.park(car)
 	end
 end
