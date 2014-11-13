a003ac8d4342ec08d256f63e0daafd9fb65e4b79
Yansen <Yansen@test.com>
2014-11-13 17:34:02 +0800

    refact smart parking boy for basic validation

--- parkinglot/spec/smart_parking_boy_spec.rb
+++ parkinglot/spec/smart_parking_boy_spec.rb
@@ -1,77 +1,20 @@
 require_relative '../rspec_helper'
 
+class SmartParkingBoyProvider
+	def initialize
+	end
+
+	def create_default
+		SmartParkingBoy.new
+	end
+
+	def create_one(parkinglots)
+		SmartParkingBoy.new(parkinglots)
+	end
+end
+
 describe "Smart Parking Boy" do
-	it "should be able park car" do
-		parking_service = SmartParkingBoy.new
-		car = Car.new
-		expect(parking_service.park(car)).to eq(true)
-	end
-
-	it "should be able to pick parked car" do
-		parking_service = SmartParkingBoy.new
-		car = Car.new
-		parking_service.park(car)
-		expect(parking_service.pick(car.id)).to eq(car)
-	end
-
-	it "should not be able to park when all parking lots are full" do
-		parkinglot1 = Parkinglot.new(0)
-		parkinglot2 = Parkinglot.new(0)
-		parking_service = SmartParkingBoy.new([parkinglot1, parkinglot2])
-
-		car = Car.new
-
-		expect(parking_service.park(car)).to eq(false)
-	end
-
-	it "should be able to pick a car which is parked by car owner" do
-		parkinglot = Parkinglot.new
-		car = Car.new
-		parkinghelper = SmartParkingBoy.new([parkinglot])
-
-		parkinglot.park(car)
-
-		expect(parkinghelper.pick(car.id)).to eq(car)
-	end
-
-	it "should not be able to pick a car which is never parked" do
-		parkinghelper = SmartParkingBoy.new
-		expect(parkinghelper.pick("GA000000")).to eq(false)
-	end
-
-	it "should not be able to pick a car again after picked" do
-		parkinghelper = SmartParkingBoy.new
-		car = Car.new
-		parkinghelper.park(car)
-
-		parkinghelper.pick(car.id)
-
-		expect(parkinghelper.pick(car.id)).to eq(false)
-	end
-
-	it "should still be able to park when only one is full but it manages two parking lots" do
-		parkinglot1 = Parkinglot.new(1)
-		parkinglot2 = Parkinglot.new(1)
-		parkinghelper = SmartParkingBoy.new([parkinglot1, parkinglot2])
-		car1 = Car.new
-		parkinghelper.park(car1)
-
-		car2 = Car.new
-		expect(parkinghelper.park(car2)).to eq(true)
-	end
-
-	it "should be able to pick correct cars when there are multiple parkinglots" do
-		parkinglot1 = Parkinglot.new(1)
-		parkinglot2 = Parkinglot.new(1)
-		parkinghelper = SmartParkingBoy.new([parkinglot1, parkinglot2])
-		car1 = Car.new
-		parkinghelper.park(car1)
-		car2 = Car.new
-		parkinghelper.park(car2)
-
-		expect(parkinghelper.pick(car1.id)).to eq(car1)
-		expect(parkinghelper.pick(car2.id)).to eq(car2)
-	end
+	parking_boy_basic_validator(SmartParkingBoyProvider.new)
 
 	describe "When there is only one parking lot" do
 		it "should park to this parking lot" do
