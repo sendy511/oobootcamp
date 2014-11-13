6229e63ee352cda767fbafb8725d2156b12a8370
Yansen <Yansen@test.com>
2014-11-13 17:28:36 +0800

    refact parking boy for futhure usage

--- parkinglot/spec/parking_boy_spec.rb
+++ parkinglot/spec/parking_boy_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../rspec_helper'
 
-def parking_boy_basic_validator
+def parking_boy_basic_validator(parking_boy_provider)
 	it "should be able park car" do
-		parking_service = ParkingBoy.new
+		parking_service = parking_boy_provider.create_default
 		car = Car.new
 		expect(parking_service.park(car)).to eq(true)
 	end
 
 	it "should be able to pick parked car" do
-		parking_service = ParkingBoy.new
+		parking_service = parking_boy_provider.create_default
 		car = Car.new
 		parking_service.park(car)
 		expect(parking_service.pick(car.id)).to eq(car)
@@ -17,7 +17,7 @@ def parking_boy_basic_validator
 	it "should not be able to park when all parking lots are full" do
 		parkinglot1 = Parkinglot.new(0)
 		parkinglot2 = Parkinglot.new(0)
-		parking_service = ParkingBoy.new([parkinglot1, parkinglot2])
+		parking_service = parking_boy_provider.create_one([parkinglot1, parkinglot2])
 
 		car = Car.new
 
@@ -27,7 +27,7 @@ def parking_boy_basic_validator
 	it "should be able to pick a car which is parked by car owner" do
 		parkinglot = Parkinglot.new
 		car = Car.new
-		parkinghelper = ParkingBoy.new([parkinglot])
+		parkinghelper = parking_boy_provider.create_one([parkinglot])
 
 		parkinglot.park(car)
 
@@ -35,12 +35,12 @@ def parking_boy_basic_validator
 	end
 
 	it "should not be able to pick a car which is never parked" do
-		parkinghelper = ParkingBoy.new
+		parkinghelper = parking_boy_provider.create_default
 		expect(parkinghelper.pick("GA000000")).to eq(false)
 	end
 
 	it "should not be able to pick a car again after picked" do
-		parkinghelper = ParkingBoy.new
+		parkinghelper = parking_boy_provider.create_default
 		car = Car.new
 		parkinghelper.park(car)
 
@@ -52,7 +52,7 @@ def parking_boy_basic_validator
 	it "should still be able to park when only one is full but it manages two parking lots" do
 		parkinglot1 = Parkinglot.new(1)
 		parkinglot2 = Parkinglot.new(1)
-		parkinghelper = ParkingBoy.new([parkinglot1, parkinglot2])
+		parkinghelper = parking_boy_provider.create_one([parkinglot1, parkinglot2])
 		car1 = Car.new
 		parkinghelper.park(car1)
 
@@ -63,7 +63,7 @@ def parking_boy_basic_validator
 	it "should be able to pick correct cars when there are multiple parkinglots" do
 		parkinglot1 = Parkinglot.new(1)
 		parkinglot2 = Parkinglot.new(1)
-		parkinghelper = ParkingBoy.new([parkinglot1, parkinglot2])
+		parkinghelper = parking_boy_provider.create_one([parkinglot1, parkinglot2])
 		car1 = Car.new
 		parkinghelper.park(car1)
 		car2 = Car.new
@@ -74,6 +74,19 @@ def parking_boy_basic_validator
 	end
 end
 
+class ParkingBoyProvider
+	def initialize
+	end
+
+	def create_default
+		ParkingBoy.new
+	end
+
+	def create_one(parkinglots)
+		ParkingBoy.new(parkinglots)
+	end
+end
+
 describe "Parking Boy" do
-	parking_boy_basic_validator
+	parking_boy_basic_validator(ParkingBoyProvider.new)
 end
