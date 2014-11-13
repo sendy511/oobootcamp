37c3f6e8f6a23b93cc286190d718c29c35f00b28
Yansen <Yansen@test.com>
2014-11-13 17:18:34 +0800

    Revert "refactor: refine the parkinglot to future usage"
    This reverts commit 35712616df7d8e5d9f649968c50b7b94684f8c2e.

--- parkinglot/spec/parkinglot_spec.rb
+++ parkinglot/spec/parkinglot_spec.rb
@@ -1,34 +1,34 @@
 require_relative '../rspec_helper'
 
-def parking_provider_basic_validation(parkinglot_provider)
+def parking_provider_basic_validation()
 	it "should be able to park car" do
-		parkinglot = parkinglot_provider.create_default
+		parkinglot = Parkinglot.new
 		car1 = Car.new
 		expect(parkinglot.park(car1)).to eq(true)
 	end
 
 	it "should be able to pick parked car" do
-		parkinglot = parkinglot_provider.create_default
+		parkinglot = Parkinglot.new
 		car1 = Car.new("GA-888888")
 		parkinglot.park(car1)
 		expect(parkinglot.pick(car1.id)).to eq(car1)
 	end
 
 	it "should not be able to park car when full" do
-		parkinglot = parkinglot_provider.create_one(0)
+		parkinglot = Parkinglot.new(0)
 		car1 = Car.new
 		expect(parkinglot.park(car1)).to eq(false)
 	end
 
 	it "should not be able to pick an unpacked car" do
-		parkinglot = parkinglot_provider.create_default
+		parkinglot = Parkinglot.new
 		car1 = Car.new("GA-888888")
 		parkinglot.park(car1)
 		expect(parkinglot.pick("GA-000000")).to eq(false)
 	end
 
 	it "should not be able to pick car again after picked" do
-		parkinglot = parkinglot_provider.create_default
+		parkinglot = Parkinglot.new()
 		car = Car.new("GA-888888")
 		parkinglot.park(car)
 		parkinglot.pick(car.id)
@@ -36,7 +36,7 @@ def parking_provider_basic_validation(parkinglot_provider)
 	end
 
 	it "should be able to park again after a car is picked when the parkinglot is full" do
-		parkinglot = parkinglot_provider.create_one(1)
+		parkinglot = Parkinglot.new(1)
 		car1 = Car.new("GA-888888")
 		car2 = Car.new("GA-666666")
 		parkinglot.park(car1)
@@ -48,22 +48,8 @@ def parking_provider_basic_validation(parkinglot_provider)
 	end
 end
 
-class ParkingLotsProvider
-	def initialize
-	end
-	
-	def create_default
-		Parkinglot.new
-	end
-
-	def create_one(capacity)
-		Parkinglot.new(capacity)
-	end
-end
-
 describe "parkinglot" do 
-	parking_provider_basic_validation(ParkingLotsProvider.new)
+	parking_provider_basic_validation
 end
-
 
 
